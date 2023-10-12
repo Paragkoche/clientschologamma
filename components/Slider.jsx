342bcd093dfaaa27c3eb5ffbd06fa2ace604308b
harycoder1 <harish7823@gmail.com>
2023-10-12 08:58:38 +0530

    initial commit half final++

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -4,7 +4,7 @@ const data = [1,2,3]
 
 const Slider1 = () => {
   return (
-<div>
+<div className="mt-[60px] md:mt-[2px]">
 <Image src={"/landingpage.jpg"} width={1900} height={1050} className="w-screen  h-auto" />
 </div>
   );
