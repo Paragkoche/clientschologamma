6d03869e5f719113a83fbe65dddce9b852077af7
harycoder1 <harish7823@gmail.com>
2023-10-15 07:46:24 +0530

    only for check

--- app/committee/page.jsx
+++ app/committee/page.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react'
 
 function Committee() {
+  useEffect(() => {
+    const fetchData = async()=>{
+      const res =await fetch('https://adminab9211.vercel.app/api/committee/')
+      const data = await res.json()
+      console.log(data)
+    }
+fetchData()
+  }, [])
+  
   return (
     <div className='flex justify-center items-center h-screen w-screen'>
     <h1 className='text-[64px] text-green-500 drop-shadow-xl '>Comming Soon...</h1>
